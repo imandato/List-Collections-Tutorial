e77a804edf583184e96e97187a95c5d3f22a3b45
imandato <imandato211@gmail.com>
2023-03-20 16:36:13 -0400

    finished tutorial and challenge

--- Program.cs
+++ Program.cs
@@ -1,1 +1,81 @@
-﻿
+﻿WorkWithStrings();
+
+void WorkWithStrings()
+{
+    var names = new List<string> { "Ivelisse", "Ana", "Felipe" };
+    foreach (var name in names)
+    {
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+    }
+
+    Console.WriteLine();
+    names.Add("Maria");
+    names.Add("Bill");
+    names.Remove("Ana");
+    foreach (var name in names)
+    {
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+    }
+
+    Console.WriteLine($"My name is {names[0]}");
+    Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+
+    Console.WriteLine($"The list has {names.Count} people in it");
+
+    var index = names.IndexOf("Felipe");
+    if (index == -1)
+    {
+        Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+    }
+    else
+    {
+        Console.WriteLine($"The name {names[index]} is at index {index}");
+    }
+
+    index = names.IndexOf("Not Found");
+    if (index == -1)
+    {
+        Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+    }
+    else
+    {
+        Console.WriteLine($"The name {names[index]} is at index {index}");
+
+    }
+
+    names.Sort();
+    foreach (var name in names)
+    {
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+    }
+}
+
+var fibonacciNumbers = new List<int> { 1, 1 };
+
+//var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+//var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+//fibonacciNumbers.Add(previous + previous2);
+
+foreach (var item in fibonacciNumbers)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine();
+
+//challenge
+
+for (int index = 0; index<18; index++)
+{
+    var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+    var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+    fibonacciNumbers.Add(previous + previous2);
+
+}
+
+foreach (var item in fibonacciNumbers)
+{
+    Console.WriteLine(item);
+}
